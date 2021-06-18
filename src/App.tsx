f31ee0fd0db78ea7657638fb0475dc9ea34079d4
Alex <AlexeyVolkov@users.noreply.github.com>
2021-06-18 15:39:45 +0300

    add last modified date

--- src/App.tsx
+++ src/App.tsx
@@ -1,46 +1,31 @@
-import debounce from './utils/debounce'
-import React from 'react'
-import './App.scss'
+import { ChangeEvent } from 'react'
 
-// Hook
-function useLocalStorage<T>(key: string, initialValue: T) {
-  // State to store our value
-  // Pass initial state function to useState so logic is only executed once
-  const [storedValue, setStoredValue] = React.useState<T>(() => {
-    try {
-      // Get from local storage by key
-      const item = window.localStorage.getItem(key)
-      // Parse stored json or if none return initialValue
-      return item ? JSON.parse(item) : initialValue
-    } catch (error) {
-      // If error also return initialValue
-      console.log(error)
-      return initialValue
-    }
-  })
-  // Return a wrapped version of useState's setter function that ...
-  // ... persists the new value to localStorage.
-  const setValue = (value: T | ((val: T) => T)) => {
-    try {
-      // Allow value to be a function so we have same API as useState
-      const valueToStore =
-        value instanceof Function ? value(storedValue) : value
-      // Save state
-      setStoredValue(valueToStore)
-      // Save to local storage
-      window.localStorage.setItem(key, JSON.stringify(valueToStore))
-    } catch (error) {
-      // A more advanced implementation would handle the error case
-      console.log(error)
-    }
-  }
-  return [storedValue, setValue] as const
-}
+import debounce from './utils/debounce'
+import useLocalStorage from './utils/useLocalStorage'
+import './App.scss'
 
 function App() {
   const [noteText, setNoteText] = useLocalStorage<string>('note-text', '')
-  const updateNoteText = (event: React.ChangeEvent<HTMLTextAreaElement>) =>
+  const [noteLastModifiedDate, setNoteLastModifiedDate] = useLocalStorage<Date>(
+    'note-date',
+    new Date()
+  )
+  const options: object = {
+    weekday: 'long',
+    year: 'numeric',
+    month: 'long',
+    day: 'numeric',
+  }
+  const dateToShow: string = new Date(noteLastModifiedDate).toLocaleDateString(
+    'en-US',
+    options
+  )
+
+  const updateNoteText = (event: ChangeEvent<HTMLTextAreaElement>) => {
     setNoteText(event.target.value)
+    setNoteLastModifiedDate(new Date())
+  }
+
   const noteChangeHandler = debounce(updateNoteText, 300)
 
   return (
@@ -55,6 +40,7 @@ function App() {
         onChange={noteChangeHandler}
         defaultValue={noteText}
       />
+      <aside className='note-text__date'>{dateToShow}</aside>
     </form>
   )
 }
