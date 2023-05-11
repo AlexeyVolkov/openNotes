2d2c7f6cde588708e40ae3939faf3747cb3a953d
SlimCandy <91788772+slimcandy@users.noreply.github.com>
2023-05-11 13:58:31 +0200

    Add wysiwyg

--- src/App.tsx
+++ src/App.tsx
@@ -1,23 +1,27 @@
-import { ChangeEvent } from "react";
+import { ChangeEvent, useRef, useState } from "react";
 import { Form, Seo, Navigation } from "./components";
 import { debounceDelay, defaultNote } from "./utils/constants";
 import { INote } from "./utils/interface";
 import useDebouncedIndexedDB from "./utils/indexedDB/useDebouncedIndexedDB";
+
+import ReactQuill from "react-quill";
+import "react-quill/dist/quill.bubble.css";
 
 function App() {
   const [note, setNote, isLoading] = useDebouncedIndexedDB(
     defaultNote,
     debounceDelay
   );
+  const editor = useRef<ReactQuill>(null);
+  const copyToClipboard = editor.current?.getEditor().getText() ?? "";
 
-  const onChange = (event: ChangeEvent<HTMLTextAreaElement>) => {
+  const onWYSIWYGChange = (value: string) => {
     const nextNote: INote = {
       ...note,
-      text: event.target.value,
+      text: value,
       updatedAt: new Date(),
       id: 1,
     };
-
     setNote(nextNote);
   };
 
@@ -28,12 +32,28 @@ function App() {
         description="OpenNote is an open-source, offline-capable note-taking application, designed to capture and share your ideas freely and efficiently, anytime, anywhere."
       />
       <header className="leading-none">
-        <Navigation text={note.text} />
+        <Navigation text={copyToClipboard} />
       </header>
-      <main className="p-safe">
-        <Form onChange={onChange} disabled={isLoading}>
-          {note.text}
-        </Form>
+      <main className="p-safe mt-2 flex justify-center items-center">
+        <ReactQuill
+          ref={editor}
+          readOnly={isLoading}
+          theme="bubble"
+          className="
+          mt-2
+          font-sans text-2xl font-thin oldstyle-nums hyphens-auto text-justify
+          border-2 rounded-lg p-4 mx-4 w-10/12"
+          value={note.text}
+          onChange={onWYSIWYGChange}
+          placeholder="Write…"
+          modules={{
+            toolbar: [
+              ["bold", "italic"], // toggled buttons
+              ["code-block"],
+              [{ list: "ordered" }, { list: "bullet" }],
+            ],
+          }}
+        />
       </main>
     </>
   );
